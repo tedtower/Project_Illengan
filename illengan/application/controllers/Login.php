11f2ae41d9b0d40f9bae1b00de96758411b795ab
tedtower <2168162@slu.edu.ph>
2019-02-05 16:09:19 +0800

    basta

--- illengan/application/controllers/Login.php
+++ illengan/application/controllers/Login.php
@@ -1,6 +1,22 @@
 <?php
 class Login extends CI_Controller{
 
+    function index(){
+        $this->load->view('landing');
+    }
+
+    function load_adminlogin(){
+        $this->load->view('admin_module/login');
+    }
+
+    function load_baristalogin(){
+        $this->load->view('barista_module/login');
+    }
+
+    function load_cheflogin(){
+        $this->load->view('chef_module/login');
+    }
+
     function check_cred(){
         $uname = $this->input->post('username');
         $pword = $this->input->post('password');
---
+++ illengan/application/views/landing.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div>
+            <a href="<?php echo base_url(); ?>/adminlogin">LOGIN as Admin</a>
+            <a href="<?php echo base_url(); ?>/baristalogin">LOGIN as Barista</a>
+            <a href="<?php echo base_url(); ?>/cheflogin">LOGIN as Chef</a>
+        </div>
+    </body>
+</html>
