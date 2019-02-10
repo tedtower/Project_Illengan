578a4d3359a34bee8a0eda04976538320f4d5e9b
tedtower <2168162@slu.edu.ph>
2019-02-10 10:58:28 +0800

    removed .htaccess

--- illengan/application/config/config.php
+++ illengan/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.illengan.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
