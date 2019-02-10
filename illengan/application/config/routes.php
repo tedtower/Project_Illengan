01523974e78016b1fa2942d0088f6763025a28b7
tedtower <2168162@slu.edu.ph>
2019-02-10 10:43:20 +0800

    Some things

--- illengan/application/config/routes.php
+++ illengan/application/config/routes.php
@@ -52,6 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['tatum'] = 'login/load_adminlogin';
+$route['adminlogin'] = 'login/load_adminlogin';
 $route['baristalogin'] = "login/load_baristalogin";
 $route['cheflogin'] = "login/load_cheflogin";
