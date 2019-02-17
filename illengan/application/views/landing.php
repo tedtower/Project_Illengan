0dd9f13b81c7b4064d5ba5dc8eebb0ac0cd46132
Jeane Cris Cariño Domaoa <2168137@slu.edu.ph>
2019-02-17 05:36:05 +0000

    Added a link for the customer module

--- illengan/application/views/landing.php
+++ illengan/application/views/landing.php
@@ -7,6 +7,7 @@
             <a href="<?php echo site_url('adminlogin')?>">LOGIN as Admin</a>
             <a href="<?php echo base_url('baristalogin'); ?>">LOGIN as Barista</a>
             <a href="<?php echo base_url('cheflogin'); ?>">LOGIN as Chef</a>
+            <a href="<?php echo base_url('custmod'); ?>">Open Ordering Module</a>
         </div>
     </body>
 </html>
