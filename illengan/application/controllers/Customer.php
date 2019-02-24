291afc3be861dc12e4d47e2cd06523a5b9c5303d
Jeane Cris Cariño Domaoa <2168137@slu.edu.ph>
2019-02-24 16:30:01 +0800

    Changed phps

--- illengan/application/controllers/Customer.php
+++ illengan/application/controllers/Customer.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Customer extends CI_Controller {
 
-	public function view($page = '') {
+	public function view($page = 'login') {
 		if(!file_exists(APPPATH.'views/customer/'.$page.'.php')) {
 			show_404();
 		}
