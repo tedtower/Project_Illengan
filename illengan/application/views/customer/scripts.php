cffbc0a1d192bc51e79ba16ebff204c56189e784
iJeane <2168137@slu.edu.ph>
2019-03-09 08:25:14 -0800

    Changed directory src

--- illengan/application/views/customer/scripts.php
+++ illengan/application/views/customer/scripts.php
@@ -1,10 +1,10 @@
 	<!-- Frameworks -->
-    <script src="<?php echo base_url().'application/js/frameworks/jquery-3.3.1.min.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/frameworks/popper.min.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/frameworks/bootstrap.min.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/frameworks/mdb.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/common/frameworks/mdb/js/jquery-3.3.1.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/common/frameworks/mdb/js/popper.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/common/frameworks/mdb/js/bootstrap.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/common/frameworks/mdb/js/mdb.min.js'?>"></script>
     <!-- Custom -->
-    <script src="<?php echo base_url().'application/js/customer/loadmodal.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/customer/progress.bar.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/customer/quantity.js'?>"></script>
-    <script src="<?php echo base_url().'application/js/customer/changtemp.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/customer/loadmodal.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/customer/progress.bar.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/customer/quantity.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/customer/changtemp.js'?>"></script>
