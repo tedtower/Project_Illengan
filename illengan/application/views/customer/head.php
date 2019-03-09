520c70e967a4e8860631722f9195c49c74343764
iJeane <2168137@slu.edu.ph>
2019-03-09 08:16:03 -0800

    Changed directory href

--- illengan/application/views/customer/head.php
+++ illengan/application/views/customer/head.php
@@ -1,12 +1,12 @@
 <!-- Webpage Icon -->
     <link rel="icon" href="<?php echo base_url().'application/css/media/logo.png'?>" type="image/png" />
     <!-- Frameworks -->
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/frameworks/bootstrap.min.css'?>">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/frameworks/mdb.min.css'?>">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/frameworks/animations-extended.min.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/frameworks/mdb/css/bootstrap.min.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/frameworks/mdb/css/mdb.min.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/frameworks/mdb/css/animations-extended.min.css'?>">
     <!-- Fonts -->
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/frameworks/font-awesome.css'?>">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/customer/gabriola.css'?>">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/customer/delius.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/frameworks/fontawesome/font-awesome.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/fontstyles/gabriola.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/common/fontstyles/delius.css'?>">
     <!-- Custom -->
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'application/css/customer/style.css'?>">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url().'assets/css/customer/style.css'?>">
