a3fe4db3e1faf09b5ba5f29d35fda6d2ec083b5a
mel0924 <2168590@slu.edu.ph>
2019-03-20 09:19:01 +0800

    updated barista ordering

--- illengan/application/controllers/Barista.php
+++ illengan/application/controllers/Barista.php
@@ -1,11 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class Barista extends CI_Controller{
-        function __construct() {        
-            parent::__construct();
-            $this->load->model('baristaModel');
-            }
+class Barista extends CI_Controller{
+    
+    function __construct(){
+        parent:: __construct();      
+        date_default_timezone_set('Asia/Manila');        
+        $this->load->model('baristamodel');  
+        // code for getting current date : date("Y-m-d")
+        // code for getting current date and time : date("Y-m-d H:i:s")
+    }
 
         function index()
         {
@@ -16,6 +20,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $data=$this->baristaModel->view();
 		    echo json_encode($data);
         }
+
+        function getOrders(){
+            if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Barista'){
+                //Code Here
+            }else{
+                redirect('login');
+            }
+        }
+
+        function getBills(){
+            if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Barista'){
+                    $data["bills"] = $this->baristamodel->get_bills();
+                    $this->load->view("barista/baristabillings", $data);
+                }else{
+                        redirect('login');
+                }
+            }
+
+         function getBillDetails(){
+             if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Barista'){
+                $order_id = $this->input->post("order_id"); 
+                $orderdetails = array(
+                'orderslip' => $this->baristamodel->get_orderslip($order_id)[0],
+                'orderlist' => $this->baristamodel->get_orderlist($order_id)
+                );
+                 $this->output->set_output(json_encode($orderdetails));
+                }else{
+                redirect('login');
+
+                }
+            }
 
         function getDate(){
             $this->load->helper('date');
@@ -62,23 +97,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             echo json_encode($data);
         }
 
+        function setBillStatus(){        
+            if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Barista'){
+                $payment_date_time = date("Y-m-d H:i:s");
+                $date_recorded = date("Y-m-d");
+                $order_id = $this->input->post("order_id");
+                $status = $this->input->post("pay_status");
+                
+                switch($status){
+                    case "p": 
+                        if($this->baristamodel->update_billstatus($order_id)){
+                            $this->output->set_output(json_encode($this->baristamodel->get_bills()));
+                        }else{
+                            //error
+                        }
+                        break;
+                    case "u" : 
+                        if($this->baristamodel->update_billstatus($order_id, $payment_date_time, $date_recorded)){
+                            $this->output->set_output(json_encode($this->baristamodel->get_bills()));
+                        }else{
+                            //error
+                        }
+                        break;
+            
+                    }else{
+                        redirect('login');
+                    }
+                }
+            }
+            
+
     }
+
         
 
-           /* $order_id = $this->input->get('order_id');
-            $table_code = $this->input->get('table_code');
-            $data['orderslip'] = $this->baristaModel->edit_tablenumber($table_code, $order_id);
-            $this->load->view('editTable', $data);
-            }
-            ---------------------------------------------------------------------------
 
-        function searchCustomer(){
-            $data['orderArray'] = $this->baristaModel->search();
-            $this->load->view('barista/searchView', $data);
-
-        }
-
-        function change_status() {
+        /*function change_status() {
             $order_id = $this->input->post('order_id');
             $menu_id = $this->input->post('menu_id');
             $item_status = $this->input->post('item_status');
--- illengan/application/models/BaristaModel.php
+++ illengan/application/models/BaristaModel.php
@@ -50,6 +50,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             $this->db->query($query, array($item_status, $order_id, $menu_id));
         }
 
+        function get_bills(){
+            $query = "select order_id, table_code, cust_name, order_payable, order_date, if(pay_date_time is null, 'Unpaid', 'Paid') as pay_status , pay_date_time from orderslip";
+            return $this->db->query($query)->result_array();
+        }
+
+        function get_orderlist($order_id){
+            $query = "Select order_item_id, menu_name, order_qty, order_total from orderlist inner join menu using (menu_id) where order_id = ?";
+            return $this->db->query($query, array($order_id))->result_array(); 
+        }
+
+        function get_orderslip($order_id){
+            $query = "select order_id, table_code, cust_name, order_payable, order_date, if(pay_date_time is null, 'Unpaid', 'Paid') as pay_status , pay_date_time from orderslip where order_id = ?";
+            return $this->db->query($query, array($order_id))->result_array();
+        }
+
+        function update_billstatus($order_id, $payment_date_time = null, $date_recorded = null){
+            $query = "update orderslip set pay_date_time = ?, date_recorded = ? where order_id=?";
+            return $this->db->query($query, array($payment_date_time, $date_recorded, $order_id));
+        }
+
     }
 
 ?>
