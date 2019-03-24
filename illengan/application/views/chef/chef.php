92ac76db485915b49fb2bf5c91ae13ca6757ecd4
JessaLyn216 <2162655@slu.edu.ph>
2019-03-24 14:59:18 +0800

    Jessa updated Chef Module

--- illengan/application/views/chef/chef.php
+++ illengan/application/views/chef/chef.php
@@ -87,12 +87,7 @@ $(document).ready(function() {
                         ' data-item_status="'+ data.item_status +'" onclick="change_status()">'+ data.item_status +'</button>';
                         }
                     }
-		        ],
-                createdRow: function (row, data, index) {
-                        console.dir(row);
-                        $(row).addClass('shown');
-
-                }
+		        ]
 	        });
 
 // FOR SHOWING THE ACCORDION
@@ -114,7 +109,9 @@ $(document).ready(function() {
         
         $("#mydata").DataTable().rows().every( function () {
                 var tr = $(this.node());
-                this.child(format(tr.data('child-value'))).show();
+                var row = table.row(tr);
+
+                row.child(format(row.data())).show();
                 tr.addClass('shown');
             });
 
