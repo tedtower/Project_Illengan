09d6de2f55cd12fba29ab07d6451c112aeb6d403
JessaLyn216 <2162655@slu.edu.ph>
2019-03-25 15:04:40 +0800

    Committed changes with the item discounts

--- illengan/assets/js/customer/discountdisp.js
+++ illengan/assets/js/customer/discountdisp.js
@@ -12,11 +12,13 @@ function promos() {
                 var menu_id;
 
                 for(i = 0; i < data.length ; i++) {
+                    if(data[i].promo_id != null) {
                     d_menu_id = data[i].menu_id; // Ito yung menu id na galing sa database
                     menu_id = document.getElementById(d_menu_id); // kinuha ko yung div na yon gamit yung value ng id
                     menu_id.style.border = '10px solid purple'; // ito na yung part na pag add ng css kung background color 
                     // ilagagay mo kunwari ganito lang menu_id.style.backgroundColor = 'black'; ganern search mo na lang yung
-                    // kung pano mag change ng css gamit javascript          
+                    // kung pano mag change ng css gamit javascript  
+                    }         
                 }
                 },
               failure: function() {
