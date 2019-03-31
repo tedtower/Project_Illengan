23efb0012ede95027448312c2feafdbaffb41a95
jegenrogosx <34230683+jegenrogosx@users.noreply.github.com>
2019-03-31 14:12:40 +0800

    Update sideNav.php

--- illengan/application/views/admin/templates/sideNav.php
+++ illengan/application/views/admin/templates/sideNav.php
@@ -3,55 +3,56 @@
         <!--Left Navigation Bar-->
         <div class="sidebar-wrapper" style="overflow: hidden">
             <div class="logo">
-                <img src="<?php echo base_url().'assets/media/admin/logo_lg.png' alt="il-lengan-logo" img-align="center" width="225px" height="135px">?>">
+                <img src="<?php echo base_url().'assets/media/admin/logo_lg.png' ?>" style="alt:il-lengan-logo; img-align: center; width:225px; height;135px;">
             </div>
 
             <ul class="nav">
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminDashboard.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminDashboard.html' ?>">
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminMenuItems.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminMenuItems.html' ?>">
                         <p>Menu Items</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminSales.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminSales.html' ?>">
                         <p>Sales</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminInventory.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminInventory.html' ?>">
                         <p>Inventory</p>
                     </a>
                 </li>
 
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminTables.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminTables.html' ?>">
                         <p>Tables</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminReports.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminReports.html' ?>">
                         <p>Reports</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminAccounts.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminAccounts.html' ?>">
                         <p>Accounts</p>
                     </a>
                 </li>
                 <li class="active">
-                    <a <?php echo base_url().'application/views/admin/adminTransactions.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminTransactions.html' ?>">
                         <p>Transactions</p>
                     </a>
                 </li>
                 <li>
-                    <a <?php echo base_url().'application/views/admin/adminSpoilages.html' ?>">
+                    <a href="<?php echo base_url().'application/views/admin/adminSpoilages.html' ?>">
                         <p>Spoilages</p>
                     </a>
                 </li>
             </ul>
-        </div> 
+        </div>
+</div>
