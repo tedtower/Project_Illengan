5b00ed52c5010a3a9d36f1a980497356832d8715
Carla <34297873+Sherlea@users.noreply.github.com>
2019-04-10 09:48:44 +0800

    edited order_modal

--- illengan/application/views/customer/modals/order_modal.php
+++ illengan/application/views/customer/modals/order_modal.php
@@ -1,4 +1,3 @@
-
  <!-- Order Modal -->
  <div class="modal fade" id="order_modal" tabindex="-1" role="dialog" aria-labelledby="orderListModal" aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
@@ -13,12 +12,13 @@
 		$cust_name = $this->session->userdata('cust_name');
         $table_no = $this->session-> userdata('table_no');
         $orders = $this->session-> userdata('orders');
+        $date = date('F d, Y');
         $now = date('Y-m-d');
-        echo form_hidden('date', $now);
-        echo form_hidden('table_no', $table_no);
-        echo form_hidden('cust_name', $cust_name);
-		echo 'Customer Name: <strong>'.$cust_name.'</strong>
-        <br>Table Number: <strong>'.$table_no['table_code'].'</strong><br>Date: <b>'.$now.'</b>';
+            echo form_hidden('date', $now);
+            echo form_hidden('table_no', $table_no);
+            echo form_hidden('cust_name', $cust_name);
+		echo '<strong>Customer Name:</h3>'.$cust_name.'
+        <br><strong>Table Code: </strong>'.$table_no['table_code'].'<br><b>Date:&nbsp;</b>'.$date.'<br>';
           if(empty($orders)) { //check if the customer did not order yet
             echo 'To order menu items click on "Save to Orderlist" Button';
 		  }else{
@@ -28,6 +28,7 @@
                         <table class="table table-sm table-hover w-responsive mx-auto delius">
                             <thead>
                                 <tr>
+                                <th scope="col">Item No.</th>
                                 <th scope="col">Menu Name</th>
                                 <th scope="col">Quantity</th>
                                 <th scope="col">Total Price</th>
@@ -37,33 +38,42 @@
                                 </tr>
                             </thead>
 							<?php 
-							 $i=1;
+                             $rowid = 0;
                              $total_qty=0;
                              $total=0;
                              foreach ($orders as $key => $item) {
                                 $id = $item['id'];
+                                $addon = json_encode($item['addons']);
+                                echo form_hidden($rowid);
 							?>
                             <tbody>
                                 <tr>
 								<?php 
-									echo form_hidden($item['id']);
-								?>
+                                    echo form_hidden($item['id']);
+                                    
+                                ?>
+                                <td><?php //echo json_encode($item);?></td>
                                 <th scope="row"><?php echo $item['name'];?></th>
                                 <td><?php echo $item['qty'];?></td>
                                 <td><?php echo $item['subtotal'];?></td>
                                 <td><?php echo $item['remarks'];?></td>
-                                <td><?php // echo $addons;?></td>
+                                <td><?php
+                                    echo $addon;
+                                ?></td>
                                 <td>
                                     <button type="button" class="btn btn-mdb-color btn-sm m-0 p-2" data-toggle="modal" data-target="#editModal">Edit</button>
-                                    <a href="<?php echo site_url();?>customer/menu/removeOrder/"$id>
-                                    <button class="btn btn-danger btn-sm m-0 p-2">Remove</button></a>
+                                    <?php include 'edit.php'; ?>    
+                                    
+                                    <button type="button" class="btn btn-danger btn-sm m-0 p-2" data-toggle="modal" data-target="#deleteModal">Remove</button>
+                                    <?php include 'remove.php'; ?>
                                 </td> 
                                 </tr>
 							<?php
                                  $total_qty += $item['qty'];
                                  $total += $item['subtotal'];
-								$i++;
-                            }
+                                 $rowid++;
+                                
+                    }
 							?>
 								<tr>
 									<td colspan="3"><h3 class="gab">Total Quantity: <?php echo $total_qty; ?></h3></td>
@@ -76,17 +86,17 @@
                     </div>
                     <div>
                         <div class="text-center">
-                            <input type="submit" data-toggle="modal" class="btn btn-green btn-md delius" data-target="#proceed_modal"value="Order Now!"></input>
+                            <button type="button" data-toggle="modal" class="btn btn-green btn-md delius" href="#proceed_modal">Order Now</button>
+                            <?php include 'orderslip.php'; ?>
                         </div>
                         <div>
-                            <a href="<?php echo base_url();?>customer/clearOrder"><button type= "button" class="btn btn-green btn-md delius">Clear</button></a>
+                            <a href="<?php echo base_url();?>customer/clearOrder"><button type= "button" class="btn btn-danger btn-md delius">Clear</button></a>
                         </div>
                     </div>
                 </div>
 				</form>
-				<?php 
-				}
-			?>
+				<?php }; ?>
             </div>
         </div>
-    </div>			
+    </div>	
+    		
