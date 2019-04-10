24a9c169adf6b8cbd38063ce0f7938263e7eec45
Maryo <licudomary@gmail.com>
2019-04-10 09:48:40 +0800

    edited menu modal

--- illengan/application/views/admin/adminAllTransactions.php
+++ illengan/application/views/admin/adminAllTransactions.php
@@ -206,7 +206,7 @@
                                                         <div class="input-group mb-3 col">
                                                             <div class="input-group-prepend">
                                                                 <span class="input-group-text" id="inputGroup-sizing-sm"
-                                                                    style="width:100px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
+                                                                    style="width:250px;background:rgb(242, 242, 242);color:rgba(48, 46, 46, 0.9);font-size:14px;">
                                                                     Transaction Date</span>
                                                             </div>
                                                             <input type="date" name="transDate" id="transDate"
