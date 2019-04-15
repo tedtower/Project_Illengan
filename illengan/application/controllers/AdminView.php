f4fe37b15eefbddfd489ff36277326d26d1087a9
Carla <34297873+Sherlea@users.noreply.github.com>
2019-04-15 22:58:07 +0800

    Added viewreturns

--- illengan/application/controllers/AdminView.php
+++ illengan/application/controllers/AdminView.php
@@ -162,20 +162,16 @@ class AdminView extends CI_Controller{
     //         redirect('login');
     //     }
     // }
-    // function viewReturns($method=null){        
-    //     if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Admin'){
-    //         switch($method){
-    //             "add":
-    //             break;
-    //             "edit":
-    //             break;
-    //             "delete":
-    //             break;
-    //         }
-    //     }else{
-    //         redirect('login');
-    //     }
-    // }
+    function viewReturns(){
+        $data['title'] = "Returns";
+        $data['returns'] = $this->adminmodel->get_returns();
+        $data['transactions'] = $this->adminmodel->get_transactions();
+        $data['stock'] = $this->adminmodel->get_stock();
+        $this->load->view('admin/templates/head',$data);
+        $this->load->view('admin/templates/sideNav');
+        $this->load->view('admin/returns', $data);
+        $this->load->view('admin/templates/scripts');
+    }
     function viewSales(){
         if($this->session->userdata('user_id') && $this->session->userdata('user_type') === 'Admin'){
             $data['title'] = "Admin Sales";
