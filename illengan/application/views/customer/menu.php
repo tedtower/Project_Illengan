95c37dc9b038fc1d22811a3d6739e2ca48ffa9ab
Carla <34297873+Sherlea@users.noreply.github.com>
2019-05-10 07:10:03 +0800

    Edited menu.php

--- illengan/application/views/customer/menu.php
+++ illengan/application/views/customer/menu.php
@@ -9,18 +9,18 @@
 <div class="menu_box">
     <!-- Subcategory -->
     <div class="d-inline-flex px-4 animated flipInX slow subcategory mb-2 delius">
-        <h5 class="subcategory_label"><?php echo $subcategories->category_name; ?></h5>
+        <h5 class="subcategory_label"><?php echo $subcategories->ctName; ?></h5>
     </div>
     <!-- Card group -->
     <div class="d-flex flex-wrap mb-4 menu_group">
         <?php foreach($menu as $items) {
-            if($subcategories->category_name == $items->category_name){ ?>
+            if($subcategories->ctName == $items->ctName){ ?>
                 <!-- Card -->
-                <div class="card cd-mw" id="<?php echo $items->menu_id; ?>">
-                    <span class="indicate_promo <?php echo $items->menu_id; ?>" style="position:absolute;right:0;top:0"><img style="width:60px;" src="<?php echo base_url('/assets/media/customer/banner.png')?>"></span>
+                <div class="card cd-mw" id="<?php echo $items->mID; ?>">
+                    <span class="indicate_promo <?php echo $items->mID; ?>" style="position:absolute;right:0;top:0"><img style="width:60px;" src="<?php echo base_url('/assets/media/customer/banner.png')?>"></span>
                     <!-- Card image -->
-                    <a href="javascript:void(0)" onclick="hide_freebies();freebies_discounts()" class="menu_card" id="<?php echo $items->menu_id; ?>">
-                        <img data-menu_id="<?php echo $items->menu_id; ?>" class="card-img-top" src="
+                    <a href="javascript:void(0)" onclick="hide_freebies();freebies_discounts()" class="menu_card" id="<?php echo $items->mID; ?>">
+                        <img data-mID="<?php echo $items->mID; ?>" class="card-img-top" src="
                         <?php
                             if(isset($items->menu_image)){
                                 echo "".cmedia_url()."menu/".$items->menu_image;
@@ -32,8 +32,8 @@
                     <!-- Card content -->
                     <div class="card-body p-0 m-0 gab">
                         <!-- Title -->
-                        <p class="text-truncate float-left menu-title" id="mt"><?php echo $items->menu_name; ?></p>
-                        <p class="float-right menu-price" id="mp"><span class="fs-15">₱</span><?php echo $items->pref_price; ?></p>
+                        <p class="text-truncate float-left menu-title" id="mt"><?php echo $items->mName; ?></p>
+                        <p class="float-right menu-price" id="mp"><span class="fs-15">₱</span><?php echo $items->prPrice; ?></p>
                     </div>
                 </div>
         <?php }} ?>
